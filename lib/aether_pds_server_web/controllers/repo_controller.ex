bd4783a23fb7e237fe890cc53da8d0da2fdf33f4
Josh Chernoff <hello@fullstack.ing>
2025-10-08 10:46:28 -0700

    wip: better cid

--- lib/aether_pds_server_web/controllers/repo_controller.ex
+++ lib/aether_pds_server_web/controllers/repo_controller.ex
@@ -401,7 +401,7 @@ defmodule AetherPDSServerWeb.RepoController do
       result = %{
         "uri" => "at://#{repo_did}/#{collection}/#{rkey}",
         "cid" => nil,
-        "validationStatus" => "unknown"
+        "validationStatus" => "invalid"
       }
 
       op = nil
@@ -428,7 +428,7 @@ defmodule AetherPDSServerWeb.RepoController do
           result = %{
             "uri" => "at://#{repo_did}/#{collection}/#{rkey}",
             "cid" => record.cid,
-            "validationStatus" => "unknown"
+            "validationStatus" => "valid"
           }
 
           op = %{
