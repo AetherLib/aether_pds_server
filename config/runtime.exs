bdb207723437ae8ebfab9977e9dec21175fb28e5
Josh Chernoff <hello@fullstack.ing>
2025-10-10 21:35:55 -0700

    wip

--- config/runtime.exs
+++ config/runtime.exs
@@ -27,7 +27,7 @@ if config_env() != :test do
     bucket: System.get_env("AWS_BUCKET"),
     access_key_id: System.get_env("AWS_ACCESS_KEY_ID"),
     secret_access_key: System.get_env("AWS_SECRET_ACCESS_KEY"),
-    region: System.get_env("AWS_REGION")
+    region: "us-east-1"
 end
 
 if config_env() == :prod do
