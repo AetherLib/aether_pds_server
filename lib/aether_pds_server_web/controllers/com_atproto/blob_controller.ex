53483b5c9f552481d0f17cdcd5e7c86947d5a089
Josh Chernoff <hello@fullstack.ing>
2025-10-11 10:23:31 -0700

    wip

--- lib/aether_pds_server_web/controllers/com_atproto/blob_controller.ex
+++ lib/aether_pds_server_web/controllers/com_atproto/blob_controller.ex
@@ -33,41 +33,24 @@ defmodule AetherPDSServerWeb.ComATProto.BlobController do
 
         case Repositories.create_blob(blob_attrs) do
           {:ok, _blob} ->
-            # IMPORTANT: Response format must match official ATProto spec
-            response = %{
-              blob: %{
-                "$type" => "blob",
-                "ref" => %{
-                  "$link" => cid
-                },
-                "mimeType" => mime_type,
-                "size" => size
-              }
-            }
+            # Blob created successfully
+            send_blob_response(conn, cid, mime_type, size)
 
-            Logger.info("Blob upload success - CID: #{cid}")
+          {:error, %Ecto.Changeset{errors: errors} = changeset} ->
+            # Check if error is due to duplicate blob (already exists)
+            case Keyword.get(errors, :repository_did) do
+              {"has already been taken", _} ->
+                # Blob already exists - this is fine, return success
+                Logger.info("Blob already exists - CID: #{cid}")
+                send_blob_response(conn, cid, mime_type, size)
 
-            # Encode JSON and send with explicit content-length
-            json_body = Jason.encode!(response)
-            content_length = byte_size(json_body)
-
-            Logger.info("Sending response - body size: #{content_length} bytes")
-            Logger.info("Response JSON: #{json_body}")
-
-            # Send response with proper headers
-            conn
-            |> register_before_send(fn conn ->
-              Logger.info("Before send callback - ensuring response is flushed")
-              conn
-            end)
-            |> delete_resp_header("content-type")
-            |> put_resp_header("content-type", "application/json; charset=utf-8")
-            |> put_resp_header("content-length", Integer.to_string(content_length))
-            |> send_resp(200, json_body)
-
-          {:error, changeset} ->
-            Logger.error("Failed to save blob metadata: #{inspect(changeset)}")
-            json(conn, %{error: "BlobMetadataFailed", message: "Failed to save metadata"})
+              _ ->
+                # Some other error
+                Logger.error("Failed to save blob metadata: #{inspect(changeset)}")
+                conn
+                |> put_status(:internal_server_error)
+                |> json(%{error: "BlobMetadataFailed", message: "Failed to save metadata"})
+            end
         end
 
       {:error, reason} ->
@@ -81,5 +64,39 @@ defmodule AetherPDSServerWeb.ComATProto.BlobController do
       [content_type | _] -> content_type
       [] -> "application/octet-stream"
     end
+  end
+
+  defp send_blob_response(conn, cid, mime_type, size) do
+    # IMPORTANT: Response format must match official ATProto spec
+    response = %{
+      blob: %{
+        "$type" => "blob",
+        "ref" => %{
+          "$link" => cid
+        },
+        "mimeType" => mime_type,
+        "size" => size
+      }
+    }
+
+    Logger.info("Blob upload success - CID: #{cid}")
+
+    # Encode JSON and send with explicit content-length
+    json_body = Jason.encode!(response)
+    content_length = byte_size(json_body)
+
+    Logger.info("Sending response - body size: #{content_length} bytes")
+    Logger.info("Response JSON: #{json_body}")
+
+    # Send response with proper headers
+    conn
+    |> register_before_send(fn conn ->
+      Logger.info("Before send callback - ensuring response is flushed")
+      conn
+    end)
+    |> delete_resp_header("content-type")
+    |> put_resp_header("content-type", "application/json; charset=utf-8")
+    |> put_resp_header("content-length", Integer.to_string(content_length))
+    |> send_resp(200, json_body)
   end
 end
