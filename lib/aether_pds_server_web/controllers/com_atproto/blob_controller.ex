c1d56784c324270d6e024f2189e5f27d05f513d4
Josh Chernoff <hello@fullstack.ing>
2025-10-10 22:55:24 -0700

    wip

--- lib/aether_pds_server_web/controllers/com_atproto/blob_controller.ex
+++ lib/aether_pds_server_web/controllers/com_atproto/blob_controller.ex
@@ -17,74 +17,92 @@ defmodule AetherPDSServerWeb.ComATProto.BlobController do
     did = conn.assigns[:current_did]
     mime_type = get_content_type(conn)
 
-    # Stream upload to MinIO while calculating CID
-    case MinioStorage.upload_blob(conn, did, mime_type) do
-      {:ok, updated_conn, cid, size, storage_key} ->
-        Logger.info("MinIO upload successful - CID: #{cid}, Size: #{size}, Storage: #{storage_key}")
+    # TEST: Return immediate response without consuming body
+    test_response = %{
+      blob: %{
+        "$type" => "blob",
+        ref: %{
+          "$link" => "bafkreitest123"
+        },
+        mimeType: mime_type,
+        size: 12345
+      }
+    }
 
-        # Store metadata in database
-        blob_attrs = %{
-          repository_did: did,
-          cid: cid,
-          mime_type: mime_type,
-          size: size,
-          storage_key: storage_key
-        }
+    Logger.info("TEST MODE: Returning response without consuming body")
+    json(conn, test_response)
 
-        case Repositories.create_blob(blob_attrs) do
-          {:ok, _blob} ->
-            response = %{
-              blob: %{
-                "$type" => "blob",
-                ref: %{
-                  "$link" => cid
-                },
-                mimeType: mime_type,
-                size: size
-              }
-            }
+    # # Stream upload to MinIO while calculating CID
+    # case MinioStorage.upload_blob(conn, did, mime_type) do
+    #   {:ok, updated_conn, cid, size, storage_key} ->
+    #     Logger.info("MinIO upload successful - CID: #{cid}, Size: #{size}, Storage: #{storage_key}")
 
-            Logger.info("Blob upload success - DID: #{did}, CID: #{cid}, Size: #{size}")
+    #     # Store metadata in database
+    #     blob_attrs = %{
+    #       repository_did: did,
+    #       cid: cid,
+    #       mime_type: mime_type,
+    #       size: size,
+    #       storage_key: storage_key
+    #     }
 
-            # Encode response
-            json_body = Jason.encode!(response)
-            Logger.info("Encoded JSON body: #{json_body} (#{byte_size(json_body)} bytes)")
+    #     case Repositories.create_blob(blob_attrs) do
+    #       {:ok, _blob} ->
+    #         response = %{
+    #           blob: %{
+    #             "$type" => "blob",
+    #             ref: %{
+    #               "$link" => cid
+    #             },
+    #             mimeType: mime_type,
+    #             size: size
+    #           }
+    #         }
 
-            # Use chunked response to work around Bandit HTTP/2 body consumption issue
-            conn =
-              conn
-              |> put_resp_content_type("application/json; charset=utf-8")
-              |> send_chunked(200)
+    #         Logger.info("Blob upload success - DID: #{did}, CID: #{cid}, Size: #{size}")
 
-            # Send the JSON body as a single chunk
-            case Plug.Conn.chunk(conn, json_body) do
-              {:ok, conn} ->
-                Logger.info("Successfully sent response chunk")
-                conn
+    #         # Encode response
+    #         json_body = Jason.encode!(response)
+    #         Logger.info("Encoded JSON body: #{json_body} (#{byte_size(json_body)} bytes)")
 
-              {:error, reason} ->
-                Logger.error("Failed to send chunk: #{inspect(reason)}")
-                conn
-            end
+    #         # Try using Plug.Conn.inform to send an early hint, then regular response
+    #         # This might help with HTTP/2 state issues
+    #         try do
+    #           result =
+    #             conn
+    #             |> delete_resp_header("transfer-encoding")
+    #             |> put_resp_content_type("application/json; charset=utf-8")
+    #             |> put_resp_header("content-length", Integer.to_string(byte_size(json_body)))
+    #             |> resp(200, json_body)
 
-          {:error, changeset} ->
-            Logger.error("Failed to save blob metadata: #{inspect(changeset)}")
+    #           # Force the response to be sent
+    #           {:ok, sent_conn} = Plug.Conn.Adapter.send_resp(result.adapter, result.status, result.resp_headers, result.resp_body)
+    #           Logger.info("Response sent via adapter - sent successfully")
+    #           %{result | adapter: sent_conn, state: :sent}
+    #         rescue
+    #           e ->
+    #             Logger.error("Failed to send response: #{inspect(e)}")
+    #             conn |> send_resp(500, Jason.encode!(%{error: "Internal server error"}))
+    #         end
 
-            conn
-            |> put_status(:internal_server_error)
-            |> json(%{
-              error: "BlobMetadataFailed",
-              message: "Blob uploaded to storage but failed to save metadata"
-            })
-        end
+    #       {:error, changeset} ->
+    #         Logger.error("Failed to save blob metadata: #{inspect(changeset)}")
 
-      {:error, reason} ->
-        Logger.error("Failed to upload blob: #{inspect(reason)}")
+    #         conn
+    #         |> put_status(:internal_server_error)
+    #         |> json(%{
+    #           error: "BlobMetadataFailed",
+    #           message: "Blob uploaded to storage but failed to save metadata"
+    #         })
+    #     end
 
-        conn
-        |> put_status(:bad_request)
-        |> json(%{error: "BlobUploadFailed", message: "Failed to upload blob to storage"})
-    end
+    #   {:error, reason} ->
+    #     Logger.error("Failed to upload blob: #{inspect(reason)}")
+
+    #     conn
+    #     |> put_status(:bad_request)
+    #     |> json(%{error: "BlobUploadFailed", message: "Failed to upload blob to storage"})
+    # end
   end
 
   defp get_content_type(conn) do
